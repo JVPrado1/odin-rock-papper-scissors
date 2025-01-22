931760c6e49900ff9dbb1fa64ff34ef6472987a2
Prado <jv_prado@outlook.com>
2025-01-22 01:59:45 -0300

    feat: adiciona a função de escolha do usuário

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-
-
-function escolhaComputador(){
-    let randomNumber = parseInt(Math.random() * 3) + 1;
-    if (randomNumber === 1) {
-        console.log(`Pedra`)
-        return `pedra`
-    } else if (randomNumber === 2) {
-        console.log(`Papel`)
-        return `papel`
-    } else if (randomNumber === 3) {
-        console.log(`Tesoura`)
-        return `tesoura`
+    function escolhaComputador() {
+        let randomNumber = parseInt(Math.random() * 3) + 1;
+        if (randomNumber === 1) {
+            console.log(`Pedra`);
+            return `pedra`;
+        } else if (randomNumber === 2) {
+            console.log(`Papel`);
+            return `papel`;
+        } else if (randomNumber === 3) {
+            console.log(`Tesoura`);
+            return `tesoura`;
+        }
     }
-}
 
+    function jogadaUsuario() {
+        let escolhaUsuario = prompt("Escolha: Pedra, Papel ou Tesoura!").toLowerCase();
 
+        if (escolhaUsuario === `papel`) {
+            return `papel`;
+        } else if (escolhaUsuario === `pedra`) {
+            return `pedra`;
+        } else if (escolhaUsuario === `tesoura`) {
+            return `tesoura`;
+        }
+    }
 
-
+   
