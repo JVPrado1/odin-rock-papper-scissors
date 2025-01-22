722ad7bccc2f58cf98897232d46d76cbf5ed0320
Prado <jv_prado@outlook.com>
2025-01-22 01:34:46 -0300

    feat: adiciona os arquivos index.html e app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+
+
+function escolhaComputador(){
+    let randomNumber = parseInt(Math.random() * 3) + 1;
+    if (randomNumber === 1) {
+        console.log(`Pedra`)
+        return `pedra`
+    } else if (randomNumber === 2) {
+        console.log(`Papel`)
+        return `papel`
+    } else if (randomNumber === 3) {
+        console.log(`Tesoura`)
+        return `tesoura`
+    }
+}
+
+console.log(escolhaComputador())
+
+
